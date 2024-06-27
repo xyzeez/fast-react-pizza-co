f3301d2ab54b141923030607a6ce3404b125fb97
xyzeez <106898953+xyzeez@users.noreply.github.com>
2024-06-28 00:56:31 +0100

    🎨 ui(app): styled cart components

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -1,4 +1,5 @@
-import { Link } from 'react-router-dom';
+import Button, { LinkButton } from '../../components/Button';
+import CartItem from './CartItem';
 
 const fakeCart = [
   {
@@ -28,14 +29,23 @@ const Cart = () => {
   const cart = fakeCart;
 
   return (
-    <div>
-      <Link to="/menu">&larr; Back to menu</Link>
+    <div className="px-4 py-3">
+      <LinkButton to="/menu">&larr; Back to menu</LinkButton>
 
-      <h2>Your cart, %NAME%</h2>
+      <h2 className="mt-7 text-xl font-semibold">Your cart, %NAME%</h2>
 
-      <div>
-        <Link to="/order/new">Order pizzas</Link>
-        <button>Clear cart</button>
+      <ul className="mt-3 divide-y divide-stone-200 border-b">
+        {cart.map((item) => (
+          <CartItem item={item} key={item.key} />
+        ))}
+      </ul>
+
+      <div className="mt-6 space-x-2">
+        <Button to="/order/new" type="primary">
+          Order pizzas
+        </Button>
+
+        <Button type="secondary">Clear cart</Button>
       </div>
     </div>
   );
