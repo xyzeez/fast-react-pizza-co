69ec37af50adf7ca2537fcca876c7757281afe4d
xyzeez <106898953+xyzeez@users.noreply.github.com>
2024-06-28 01:08:59 +0100

    🎨 ui(app): styled user components

--- src/features/user/CreateUser.jsx
+++ src/features/user/CreateUser.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import Button from '../../components/Button';
 
 const CreateUser = () => {
   const [username, setUsername] = useState('');
@@ -9,18 +10,21 @@ const CreateUser = () => {
 
   return (
     <form onSubmit={handleSubmit}>
-      <p>👋 Welcome! Please start by telling us your name:</p>
+      <p className="mb-4 text-sm text-stone-600 md:text-base">
+        👋 Welcome! Please start by telling us your name:
+      </p>
 
       <input
         type="text"
         placeholder="Your full name"
         value={username}
         onChange={(e) => setUsername(e.target.value)}
+        className="input mb-8 w-72"
       />
 
       {username !== '' && (
         <div>
-          <button>Start ordering</button>
+          <Button type="primary">Start ordering</Button>
         </div>
       )}
     </form>
---
+++ src/features/user/Username..jsx
@@ -0,0 +1,5 @@
+const Username = () => {
+  return <div className="hidden text-sm font-semibold md:block">Jonas</div>;
+};
+
+export default Username;
