a5d1893319c7742a2818800a2178fe371ad94bce
xyzeez <106898953+xyzeez@users.noreply.github.com>
2024-06-29 00:10:53 +0100

    ✨ feat(app): implementing adding new cart item

---
+++ src/features/cart/cartSlice.js
@@ -0,0 +1,41 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  cart: [],
+};
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {
+    addItem: (state, action) => {
+      state.cart.push(action.payload);
+    },
+    deleteItem: (state, action) => {
+      state.cart = state.cart.filter((item) => item.pizzaId !== action.payload);
+    },
+    increateItemQuantity: (state, action) => {
+      const item = state.cart.find((item) => item.pizzaId === action.payload);
+      item.quantity++;
+      item.totalPrice = item.quantity * item.unitPrice;
+    },
+    decreaseItemQuantity: (state, action) => {
+      const item = state.cart.find((item) => item.pizzaId === action.payload);
+      item.quantity--;
+      item.totalPrice = item.quantity * item.unitPrice;
+    },
+    clearCart: () => {
+      return { ...initialState };
+    },
+  },
+});
+
+export const {
+  addItem,
+  deleteItem,
+  increateItemQuantity,
+  decreaseItemQuantity,
+  clearCart,
+} = cartSlice.actions;
+
+export default cartSlice.reducer;
--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,8 +1,23 @@
+import { useDispatch } from 'react-redux';
 import Button from '../../components/Button';
 import { formatCurrency } from '../../utils/helpers';
+import { addItem } from '../cart/cartSlice';
 
 const MenuItem = ({ pizza }) => {
+  const dispatch = useDispatch();
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+
+  const handleAddToCart = () => {
+    const newItem = {
+      pizzaId: id,
+      name,
+      quantity: 1,
+      unitPrice,
+      totalPrice: unitPrice * 1,
+    };
+
+    dispatch(addItem(newItem));
+  };
 
   return (
     <li className="flex gap-4 py-2">
@@ -24,7 +39,7 @@ const MenuItem = ({ pizza }) => {
               Sold out
             </p>
           )}
-          <Button type="small">Add to cart</Button>
+          {!soldOut && <Button type="small">Add to cart</Button>}
         </div>
       </div>
     </li>
