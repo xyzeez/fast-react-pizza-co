3e43bdf63331a37cbcaf4523c6203103b90794e2
xyzeez <106898953+xyzeez@users.noreply.github.com>
2024-06-30 18:36:14 +0100

    ✨ feat(app): added geolocation feature

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -2,11 +2,12 @@ import { useState } from 'react';
 import { Form, redirect, useActionData, useNavigation } from 'react-router-dom';
 import { createOrder } from '../../services/apiRestaurant';
 import Button from '../../components/Button';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { clearCart, getCart, getCartTotalPrice } from '../cart/cartSlice';
 import EmptyCart from '../cart/EmptyCart';
 import store from '../../store';
 import { formatCurrency } from '../../utils/helpers';
+import { fetchAddress } from '../user/userSlice';
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -16,9 +17,17 @@ const isValidPhone = (str) =>
 
 const CreateOrder = () => {
   const navigation = useNavigation();
+  const dispatch = useDispatch();
   const isSubmitting = navigation.state === 'submitting';
   const formErrors = useActionData();
-  const { username } = useSelector((store) => store.user);
+  const {
+    username,
+    status,
+    position,
+    address,
+    error: addressError,
+  } = useSelector((store) => store.user);
+  const isFetchAddress = status === 'loading';
   const [withPriority, setWithPriority] = useState(false);
   const cart = useSelector(getCart);
   const totalCartPrice = useSelector(getCartTotalPrice);
@@ -56,16 +65,36 @@ const CreateOrder = () => {
           </div>
         </div>
 
-        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+        <div className="relative mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
           <label className="sm:basis-40">Address</label>
           <div className="grow">
             <input
               className="input w-full"
               type="text"
               name="address"
+              defaultValue={address}
+              disabled={isFetchAddress}
               required
             />
+            {addressError && (
+              <p className="mt-2 rounded-md bg-red-100 p-2 text-xs text-red-700">
+                {addressError}
+              </p>
+            )}
           </div>
+          {!position.latitude && !position.longitude && (
+            <div className="absolute right-[3px] top-[3px] md:right-[5px] md:top-[5px] z-50">
+              <Button
+                type="small"
+                disabled={isFetchAddress}
+                clickHandler={(e) => {
+                  e.preventDefault();
+                  dispatch(fetchAddress());
+                }}>
+                Get your location
+              </Button>
+            </div>
+          )}
         </div>
 
         <div className="mb-12 flex items-center gap-5">
@@ -84,7 +113,14 @@ const CreateOrder = () => {
 
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-          <Button disabled={isSubmitting} type="primary">
+          {position.latitude && position.longitude && (
+            <input
+              type="hidden"
+              name="position"
+              value={`${position.latitude},${position.longitude}`}
+            />
+          )}
+          <Button disabled={isSubmitting || isFetchAddress} type="primary">
             {isSubmitting
               ? 'Placing order....'
               : `Order now for ${formatCurrency(totalPrice)}`}
